2a2749c8ac7ae41e373187f741a0442c035df8e9
Kaodinna <chijiokenwoye64@gmail.com>
2025-04-13 18:37:11 +0100

    first push

--- src/pages/api/user/kyc-status.ts
+++ src/pages/api/user/kyc-status.ts
@@ -1,15 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@/util/supabase/api";
-import prisma from "@/lib/prisma";
+
 // import * as admin from "firebase-admin"; // Correct import for Firebase Admin SDK
 import { initializeApp, cert, getApp } from "firebase-admin/app";
 import { admin } from "@/lib/firebaseAdmin";
 import { firestore } from "firebase-admin";
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      require("../../../../coact-1e258-firebase-adminsdk-fbsvc-7fd02402cb.json")
-    ),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 }
 
